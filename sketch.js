eedadbcc3174ea3e7acc2050d663794d52527870
Brynn Lewin <bhl21@fsu.edu>
2024-10-14 15:00:38 -0400

    forest working

--- sketch.js
+++ sketch.js
@@ -1,53 +1,60 @@
-let forest;
-let forestSpeed = 1; // normal speed of video
+let forestSpeed = 10; // normal speed of video
+
+let forestFrames = [];
 
 let bear;
 let bearSpeed = 1;
 let showBear = false; 
+let currentForestFrame = 1;
 
 let dash;
 let dashSpeed = 1;
 
+function preload() {
+  for (i = 1; i <= 20; i++) {
+    forestFrames[i] = loadImage("assets/frame" + i + ".png")
+  }
+}
+
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  forest = createVideo("libraries/forest.MP4"); //replace this with animation later
-  forest.loop();
-  forest.hide(); 
 
-  bear = createVideo("libraries/bear.PNG");
-
-  //dash = createVideo("");
-  //dash.loop();
 }
 
 function draw() {
   background(220);
-  image(forest, 0, 0, width, height);
+  if (frameCount % forestSpeed == 0) {
+    currentForestFrame = currentForestFrame + 1;
+    if (currentForestFrame == 21) currentForestFrame = 1;
+  }
+  console.log(currentForestFrame);
+  image(forestFrames[currentForestFrame], 0, 0, width, height);
 
   // fixes framerate based on the videoSpeed
-  forest.speed(forestSpeed);
+  //forest.speed(forestSpeed);
 
-  // Checks for fastest speed first
+  // // Checks for fastest speed first
   if (keyIsDown(82) || keyIsDown(68) || keyIsDown(88) || keyIsDown(69) || keyIsDown(83) || keyIsDown(90) || keyIsDown(87) || keyIsDown(65) || keyIsDown(81)) {
-    forestSpeed = 10; 
-  }
-  // Checks for medium speed second
-  else if (keyIsDown(85) || keyIsDown(72) || keyIsDown(66) || keyIsDown(89) || keyIsDown(71) || keyIsDown(86) || keyIsDown(84) || keyIsDown(70) || keyIsDown(67)) {
-    forestSpeed = 5; 
-  }
-  // Checks for base speed
-  else if (keyIsDown(80) || keyIsDown(76) || keyIsDown(79) || keyIsDown(73) || keyIsDown(75) || keyIsDown(77) || keyIsDown(74) || keyIsDown(78)) {
-    forestSpeed = 1; 
-  }
+     forestSpeed = 10; 
+   }
+  // // Checks for medium speed second
+   else if (keyIsDown(85) || keyIsDown(72) || keyIsDown(66) || keyIsDown(89) || keyIsDown(71) || keyIsDown(86) || keyIsDown(84) || keyIsDown(70) || keyIsDown(67)) {
+     forestSpeed = 5; 
+   }
+  // // Checks for base speed
+   else if (keyIsDown(80) || keyIsDown(76) || keyIsDown(79) || keyIsDown(73) || keyIsDown(75) || keyIsDown(77) || keyIsDown(74) || keyIsDown(78)) {
+     forestSpeed = 1; 
+   }
 
-  if (showBear){
-    image(bear, 0, 0, width, height);
-  }
+  // if (showBear){
+  //   image(bear, 0, 0, width, height);
+  // }
 }
 
-function keyPressed() {
-  if (key == "1") {
-    showBear = ! showBear;
-  }
+// function keyPressed() {
+//   if (key == "1") {
+//     showBear = ! showBear;
+//   }
 
-}
+// }
